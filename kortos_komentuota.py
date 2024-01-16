907f03531c9405248429349822a9a83437ea54fe
visrim29 <r.visagurskis@gmail.com>
2024-01-16 13:52:54 +0200

    Pridėtas atskiras failas su komentarais

--- kortos_komentuota.py
+++ kortos_komentuota.py
@@ -1,27 +1,35 @@
-
+# Importuojame integruotą python modulį random, kad galėtume kviesti random modulyje esančias funkcijas.
 import random
 
+# Objektas korta.
 class Card:
-    
+
+# Kintamasis/sąrašas, kuriame nurodyti kortų rangai/reikšmės.
     card_rank = [None, None, '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    
+
+# Kintamasis/sąrašas, kuriame nurodytos kortų rūšys.
     card_suit = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
 
+# Konstruktorius/metodas __init__, jame nurodyti pradiniai kintamieji: (rank, suit, weight) ir jų atributai: (int = 0).
     def __init__(self, rank: int = 0, suit: int = 0, weight: int = 0):
         self.rank = rank
         self.suit = suit
         self.weight = weight
 
+# Metodas __repr__ reprezentuoja (spausdina) card_print kintamąjame nurodytą tekstą ir laužtiniuose skliaustuose {} nurodytas reikšmes. 
     def __repr__(self):
         card_print = f'{self.card_rank[self.rank]} of {self.card_suit[self.suit]}'
         return card_print
 
+# Metodas rank tiesiogiai grąžina sąrašo card_rank reikšmes.  
     def rank(self, card_rank):
         return card_rank
 
+# Metodas suit tiesiogiai grąžina sąrašo card_suit reikšmes.   
     def suit(self, card_suit):
         return card_suit
 
+ # Metodas sign priskiria kortų rūšim atitinkamus unicode paveikslėlius. 
     def sign(self):
         if self.suit == 'Spades':
             return u"\u2660"
@@ -32,17 +40,23 @@ class Card:
         elif self.suit == 'Diamonds':
             return u'\u2666'
 
+# Metodas naudoja for ciklą, kad priskirtume kortoms taškus.
     def weight(self, rank):
+
+# Ciklas for sunumeruoja self.rank() turinį iteruodamas indeksą (i) ir kintamąjį (card_rank). 
+# Šioje vietoje grąžintą indeksą (i) interpretuojame kaip atitinkamus kortos taškus.
         for i, card_rank in enumerate(self.rank()):
             if card_rank == rank:
                 return i
-
-
+        
+# Objektas.
 class Deck:
-    
+
+# Konstruktorius/metodas __init__, jame nurodyti pradiniai kintamieji.
     def __init__(self, deck=None):
         self.deck = deck if deck is not None else []
 
+# Metodas deck_creation sukuria išmaišytą kaladę.
     def deck_creation(self, setting = 6):
         self.deck = []
         for rank in range(setting, 15):
@@ -50,9 +64,11 @@ class Deck:
                 self.deck.append(Card(rank, suit, rank))
         random.shuffle(self.deck)
 
+# Metodas išmaišo kaladę.
     def shuffle(self):
         random.shuffle(self.deck)
 
+# Metodas grąžina kortą paimtą nuo kaladės viršaus.
     def take_top(self):
         if self.deck:
             t_card = self.deck.pop(0)
@@ -61,22 +77,26 @@ class Deck:
         else:
             print("Deck is empty.")
             return None
-            
+          
+# Metodas grąžina kortą paimtą iš kaladės apačios.   
     def take_bottom(self):
         b_card = self.deck.pop(-1)
         print(b_card)
         return b_card
 
+# Metodas grąžina atsitiktinę kortą iš kaladės
     def take_random(self):
         r_number = random.randint(0, len(self.deck)-1)
         r_card = self.deck.pop(r_number)
         print(r_card)
         return r_card
 
+# Metodo ciklas atspausdina visas kaladėje esančias kortas bei jų svorį/taškus.
     def card_weight_check_all(self):
         for card in self.deck:
             print(f'{card} - {card.weight}\n')
-     
+
+# Metodas patikrina ar dviejų kortų rūšis yra vienoda      
     def card_suit_check(self, card1: Card, card2: Card):
         return card1.suit == card2.suit
 
@@ -199,19 +219,3 @@ game.deal_cards()
 print(len(deck.deck))
 print(deck.deck)
 
-
-# UŽDUOTIS:
-# Kortų kaladė
-# Korta: Objektas (Class)
-# def __init__
-# def rank (2-9, T, J, Q, K, A)
-# def suit (spades, clubs, hearts, diamonds)
-# def sign (suit + rank)
-# def weight
-# Kortų kaladė: Objektas (Class)
-# def deck - kortų sąrašas []
-# def shuffle
-# def take from top
-# def take from bottom
-# def take random
-# Mąstom apie žaidimą
